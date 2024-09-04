74a508a45c5fa4e6eef938c92e8d15693d8af7e9
Davidson Abimbola <108395763+Davidsonabimbola@users.noreply.github.com>
2024-09-04 04:32:26 +0300

    before each hook commented out

--- cypress/e2e/homePage.cy.js
+++ cypress/e2e/homePage.cy.js
@@ -9,19 +9,19 @@ describe('template spec', () => {
         return false; // Prevents Cypress from failing the test
       }
       // if you want to ignore other errors as well, handle them here
-      var Email = 'stalkerdaveman007@yahoo.com'
-    var passWord = '34.Stalkguru'
-    cy.get('[id="email"][name="login[username]"').type(Email)
-    cy.get('[id="pass"][name="login[password]"').type(passWord)
+    //   var Email = 'stalkerdaveman007@yahoo.com'
+    // var passWord = '34.Stalkguru'
+    // cy.get('[id="email"][name="login[username]"').type(Email)
+    // cy.get('[id="pass"][name="login[password]"').type(passWord)
 
-    cy.get('button[id="send2"]').contains('Sign In').as('signIn_button')
-    cy.get('@signIn_button').click()
+    // cy.get('button[id="send2"]').contains('Sign In').as('signIn_button')
+    // cy.get('@signIn_button').click()
     });
   });
  
   it('passes', () => {
-    // var Email = 'stalkerdaveman007@yahoo.com'
-    // var passWord = '34.Stalkguru'
+    var Email = 'stalkerdaveman007@yahoo.com'
+    var passWord = '34.Stalkguru'
     var ProductName = 'Apple iPhone 15 Pro 256GB Single Sim'
     var Category = 'Phones and Tablets'
     cy.visit('https://slot.ng')
@@ -29,11 +29,11 @@ describe('template spec', () => {
 cy.wrap(header_list).get('li').get('a').contains('My Account').click()
     })
 
-    // sign in by existing user
-    // cy.get('[id="email"][name="login[username]"').type(Email)
-    // cy.get('[id="pass"][name="login[password]"').type(passWord)
-    // cy.get('button[id="send2"]').contains('Sign In').as('signIn_button')
-    // cy.get('@signIn_button').click()
+    //sign in by existing user
+    cy.get('[id="email"][name="login[username]"').type(Email)
+    cy.get('[id="pass"][name="login[password]"').type(passWord)
+    cy.get('button[id="send2"]').contains('Sign In').as('signIn_button')
+    cy.get('@signIn_button').click()
 
     placeOrder.checkProducts(Category)
     placeOrder.selectProduct(ProductName)
